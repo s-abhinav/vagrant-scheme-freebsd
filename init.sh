53661722e08cf9e9b623f9d4e114a694cd523ea6
Abhinav <abhinav1587@gmail.com>
2017-08-06 22:49:28 -0400

    1. Set wombat theme in .emacs 2. Cleanup provisioning script 3. hide ~/lisp -> ~/.lisp

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-set -x
-# echo Get the base system up to date
-# sudo pkg update && sudo pkg -y upgrade && sudo pkg autoclean -y && sudo pkg autoremove -y
 
 echo Install required apps
 sudo pkg install -y git screen wget curl emacs25 guile2
@@ -19,8 +16,8 @@ export LC_CTYPE=en_US.UTF-8
 EOF
 
 echo Install emacs utilities
-mkdir ~/lisp
-cd ~/lisp
+mkdir ~/.lisp
+cd ~/.lisp
 git clone http://git.sv.gnu.org/r/geiser.git
 git clone http://mumble.net/~campbell/git/paredit.git
 wget https://raw.githubusercontent.com/emacsmirror/emacswiki.org/master/lacarte.el
@@ -28,10 +25,12 @@ wget https://raw.githubusercontent.com/emacsmirror/emacswiki.org/master/lacarte.
 echo configure .emacs to load geiser on start
 
 cat << "EOF" > ~/.emacs
-(load-file "~/lisp/geiser/elisp/geiser.el")
+(load-theme 'wombat t)
+
+(load-file "~/.lisp/geiser/elisp/geiser.el")
 (setq geiser-active-implementations '(guile))
 
-(load-file "~/lisp/paredit/paredit.el")
+(load-file "~/.lisp/paredit/paredit.el")
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
@@ -40,7 +39,7 @@ cat << "EOF" > ~/.emacs
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
-(load-file "~/lisp/lacarte.el")
+(load-file "~/.lisp/lacarte.el")
 (global-set-key [?\e ?\M-x] 'lacarte-execute-command)
 (global-set-key [?\M-`] 'lacarte-execute-command)
 EOF
